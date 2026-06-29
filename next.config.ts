5941a39beb37fffee87c7c7f4ed8b5bdd9c7a4e0
yungwaris <ansari14w@gmail.com>
2026-06-29 17:54:53 +0530

    landing page revert

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        // Matches the root URL (yourdomain.com) and secretly loads your Framer landing page
-        source: '/',
-        destination: 'https://smiling-technology-410249.framer.app/',
-      },
-      {
-        // Safely paths other sub-pages built on Framer (like /pricing or /about) if you add them later
-        source: '/:path(about|pricing|contact)',
-        destination: 'https://smiling-technology-410249.framer.app/:path',
-      },
-    ];
-  },
+
+  /* config options here */
+
 };
+
+
 
 export default nextConfig;
